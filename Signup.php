4cb3ea216f1771cc6cded1d72310d6aa1b6de556
goldshajan <suvaringold@gmail.com>
2020-04-01 11:30:52 +0200

    changes

--- Signup.php
+++ Signup.php
@@ -28,8 +28,8 @@
             $stmt = $connection->prepare("INSERT INTO ppl(First_Name,Second_Name,Age,UserName,Password,Nationality) VALUES(?,?,?,?,?,?)");
 
             $hashedPassword = password_hash($_GET["Password"], PASSWORD_BCRYPT);
-            
-            $stmt->bind_param("ssissi", $_GET["FirstName"], $_GET["LastName"], $_GET["Age"], $_GET["Username"], $_GET["Password"], $_GET["Country"]);
+
+            $stmt->bind_param("ssissi", $_GET["FirstName"], $_GET["LastName"], $_GET["Age"], $_GET["Username"], $hashedPassword, $_GET["Country"]);
             $stmt->execute();
             print "Yaaay you have registered. Check the database <BR>";
         }
